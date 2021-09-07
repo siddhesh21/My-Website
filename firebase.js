81565dfd6c9c16cee78a40cb9b7c4c963c882d72
Siddhesh Kankekar <kankekas@tcd.ie>
2021-09-07 14:11:32 +0100

    updated with Firebase config

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAKi7TJy7aksRiOIYSxn9Ufl7iBQiq-TwA",
+    authDomain: "sid-website.firebaseapp.com",
+    projectId: "sid-website",
+    storageBucket: "sid-website.appspot.com",
+    messagingSenderId: "141113953780",
+    appId: "1:141113953780:web:e7cf4c58d7ac85c2b2fda1",
+    measurementId: "G-KHFFB7FPK6"
+  };
